7183323381ec27e487b3ab0672388ac121b7cf73
Dmitry Astankov <astankov.dmitry@gmail.com>
2022-02-10 13:36:56 +0300

    Fix mypy errors

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from src.ranker import textrank
 
 
-def test_ranker_works_correctly():
-    expected_output = []
+def test_ranker_works_correctly() -> None:
+    expected_output: List[str] = []
     documents = ["hello", "goodbye"]
     output = textrank(documents)
     assert expected_output == output, f"Expected empty output but got {output}"
