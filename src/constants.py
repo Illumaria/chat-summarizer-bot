4879544709145038d33414a53c61481d38462463
Dmitry Astankov <astankov.dmitry@gmail.com>
2022-02-14 11:38:02 +0300

    Change Secure Connect Bundle initialization method

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,7 @@ PORT = int(os.environ.get("PORT", 5000))
 
 ASTRA_DB_KEYSPACE = "chat_summarizer_bot"
 ASTRA_DB_TABLE_NAME = "messages"
-ASTRA_DB_SCB_PATH = os.environ.get("ASTRA_DB_SCB_PATH")
+ASTRA_DB_SCB_URL = os.environ.get("ASTRA_DB_SCB_URL")
 ASTRA_DB_USERNAME = os.environ.get("ASTRA_DB_USERNAME")
 ASTRA_DB_PASSWORD = os.environ.get("ASTRA_DB_PASSWORD")
 
