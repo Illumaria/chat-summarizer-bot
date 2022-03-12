15bb0ee267e90dc78b5e772af914164b46e82ac6
Dmitry Astankov <astankov.dmitry@gmail.com>
2022-03-12 19:16:10 +0300

    Message summary (#23)
    * Implement history summarization

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -4,7 +4,7 @@ from src.ranker import textrank
 
 
 def test_ranker_works_correctly() -> None:
-    expected_output: List[str] = []
+    expected_output: List[int] = [0, 1]
     documents = ["hello", "goodbye"]
     output = textrank(documents)
     assert expected_output == output, f"Expected empty output but got {output}"
